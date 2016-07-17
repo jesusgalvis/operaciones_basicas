7b4e1dda8fc29622f0f715a4b40b1cafafe75a57
jesusgalvis <galvis.1985@gmail.com>
2016-07-17 12:43:26 -0400

    Add files via upload

---
+++ Cuarto Programa suma,resta,multiplicación y División.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+	float a,b;
+	printf("ingrese dos numeros separados por enter: ");
+	scanf("%f %f",&a,&b);
+	printf("la Suma de %.2f mas %.3f es %.2f \n",a,b,a+b);
+	printf("la Resta de %.2f menos %.2f es %.2f \n",a,b,a-b);
+	printf("la Multiplicacion de %.2f por %.2f es %.2f \n",a,b,a*b);
+	printf("la Divicion de %.2f entre %.2f es %.2f \n",a,b,a/b);
+	return 0;
+}
